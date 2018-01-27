94d07acc530985986923b00ee937c71078255cf5
Jeremy LeCroy <jeremy212@gmail.com>
2018-01-26 20:27:39 -0500

    init commit

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook():
+	# 'message' is an object that represents a single GroupMe message.
+	message = request.get_json()
+
+	# TODO: Your bot's logic here
+	print(message)
+
+	return "ok", 200
